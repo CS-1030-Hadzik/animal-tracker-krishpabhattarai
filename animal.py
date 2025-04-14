6595b22c9beec677e17fe65b3b6000bd02de42b0
krishpabhattarai <krishpabhattarai@mail.weber.edu>
2025-04-14 13:12:26 -0600

    ADD: method to make generic sound

--- animal.py
+++ animal.py
@@ -16,6 +16,8 @@ class Animal:
 # TODO: Add a method to make a generic sound 
 # Call the method `speak` and make it output a specific message like 
 # "The animal makes a noise.""
+    def speak(self):
+        return "The animal makes a noise."
 
 # TODO __str__ method for string representation
 # Example output
