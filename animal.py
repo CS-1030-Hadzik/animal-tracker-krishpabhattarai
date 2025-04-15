340a17efc04340ab5696e91c3e35588aef6f61fa
krishpabhattarai <krishpabhattarai@mail.weber.edu>
2025-04-14 22:31:44 -0600

    CORRECTED: strings for both print and return function

--- animal.py
+++ animal.py
@@ -9,19 +9,19 @@ class Animal:
       
 # TODO create the initializer for Animal with name and species as attributs
     def __init__(self, name, species):
-                self.name = name
-                self.species = species
-                Animal.all_animals.append(self)
+        self.name = name
+        self.species = species
+        Animal.all_animals.append(self)
 
 # TODO: Add a method to make a generic sound 
 # Call the method `speak` and make it output a specific message like 
 # "The animal makes a noise.""
     def speak(self):
-        return "The animal makes a noise."
+        print("The animal makes a noise.")
 
 # TODO __str__ method for string representation
 # Example output
 # Kingdom: 'kingdom attribute', Name: 'name attribute' Species: 'species attribute' 
     def __str__(self):
-        return f"Kingdom: {self.kingdom}, Name: {self.name}, Species: {self.species}"
+        return f"Kingdom: '{self.kingdom}', Name: '{self.name}', Species: '{self.species}'"
 
